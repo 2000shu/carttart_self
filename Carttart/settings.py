aa8393c1617e263db298df3e8e52c06c03db63bc
Shubhendu2000 <shubhendushekhar031@gmial.com>
2025-04-09 14:59:53 +0530

    first commit

--- Carttart/settings.py
+++ Carttart/settings.py
@@ -12,18 +12,19 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hs!$k#$l3x)9f*x6opxcc606bq_da*r&zv69yij^+g)ecrx$!m'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -50,7 +51,6 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'oauth.User'
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -68,8 +68,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # Directory for static files during development
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory for static files in production
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -91,7 +92,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Carttart.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -101,7 +101,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -121,7 +120,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -133,15 +131,27 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR/"static",
-# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Session Configuration
+
+# Custom session ID cookie name
+SESSION_COOKIE_NAME = "carttart_sessionid"
+
+# Session will expire after 1 week of inactivity
+SESSION_COOKIE_AGE = 60 * 60 * 24 * 7  # 1 week
+
+# Session will persist even after the browser is closed
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+
+# Session will be saved on every request to refresh expiration
+SESSION_SAVE_EVERY_REQUEST = True
+
+# Additional security options
+SESSION_COOKIE_SECURE = False  # Change to True in production with HTTPS
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = 'Lax'
